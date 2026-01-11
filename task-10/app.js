fa4cbde0c89ec56249f687c651a5a6a469cbfb66
saleemsafdar <saleemsafdar542@gmail.com>
2026-01-11 21:59:42 +0500

    update

--- task-10/app.js
+++ task-10/app.js
@@ -1,4 +1,4 @@
-let friends = [ "saleem", "hammad", "ghazali", "zeeshan", "ammar" ];
+let friends = [ "saleem", "saad", "ghazali", "saif", "ammar" ];
 friends.forEach(friends => {
     console.log(`Aslamualikum ${friends} happy new year mubark allah bless you and inshallah this one year  you will be sucess`);
     
